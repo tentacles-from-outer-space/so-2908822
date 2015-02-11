8c3b8d5f363a20970f287c945c318af65dcc1bdf
Marek <give_me_more_spam+github@gazeta.pl>
2015-02-11 23:44:48 +0100

    Translation pl_PL -> en_??
    Comments, function and variable names, etc.

--- sym.R
+++ sym.R
@@ -1,36 +1,36 @@
 source("funs.R")
 # ============================================================
-# Ostateczne symulacje do odpowiedzi:
-funkcje <- list(dayloop2, dayloop2_A, dayloop2_B, dayloop2_C, dayloop2_D)
-f_nazwy <- c("dayloop2","dayloop2_A","dayloop2_B","dayloop2_C","dayloop2_D")
+# simulations:
+f_functions <- list(dayloop2, dayloop2_A, dayloop2_B, dayloop2_C, dayloop2_D)
+f_names <- c("dayloop2","dayloop2_A","dayloop2_B","dayloop2_C","dayloop2_D")
 
 params <- unique(rbind(
-	expand.grid(metoda_id=1L:2L,i=(1L:10L)*1000L),
-	expand.grid(metoda_id=2L:5L,i=(1L:10L)*10000L),
-	expand.grid(metoda_id=3L:5L,i=50000+(1L:8L)*100000L)
+	expand.grid(method_id=1L:2L,i=(1L:10L)*1000L),
+	expand.grid(method_id=2L:5L,i=(1L:10L)*10000L),
+	expand.grid(method_id=3L:5L,i=50000+(1L:8L)*100000L)
 ))
 params <- params[sample(seq_len(nrow(params))),]
 
 n <- 850000
 X <- as.data.frame(matrix(sample(1:10, n*9, TRUE), n, 9))
 
-czasy <- apply(params, 1, function(x) {
+times <- apply(params, 1, function(x) {
 	pX <- X[1:x[2],]
-	pF <- funkcje[[x[1]]]
+	pF <- f_functions[[x[1]]]
 	system.time(pF(pX))[1]
 })
 
-wyniki <- data.frame(metoda=f_nazwy[params$metoda_id], params, czas=czasy)
-wyniki <- wyniki[with(wyniki,order(metoda_id,i)),]
-save(wyniki, file="symulacje.RData")
+results <- data.frame(method=f_names[params$method_id], params, time=times)
+results <- results[with(results,order(method_id,i)),]
+save(results, file="results.RData")
 # ============================================================
-# Wykresy
+# Plots
 require(RColorBrewer) # display.brewer.all()
 palette(brewer.pal(9,"Set1"))
 w_axis <- function(n) axis(1, at=n*(0:10),
 	labels=prettyNum(as.integer(n*(0L:10L)), big.mark=",", preserve.width="individual"),las=1,cex.axis=.8)
 w_plot_init <- function(n, m, n0=1000) plot(0, 0,
-	ylim = with(wyniki, range(czas[metoda_id %in% m & i<= n])),
+	ylim = with(results, range(time[method_id %in% m & i<= n])),
 	xlim = c(n0, n),
 	type="n", ylab="user time (sec)", xlab="nrow(X)", las=2, xaxt="n", cex.axis=.8
 	)
@@ -44,59 +44,59 @@ text_pos <- list(
 	y3 = c(20,5,1.9,1.3,0.4)
 )
 
-# Wykres 1
+# Plot 1
 m <- 1L:2L
-png("wer_1.png",540,360)
+png("ver_1.png",540,360)
 w_plot_init(10000, m)
 w_axis(1000)
 grid()
 for (k in m) {
-	indx <- with(wyniki,metoda_id==k & i<=10000)
-	with(wyniki, lines(i[indx],czas[indx],type="b",col=k))
-	text(text_pos$x1[k],text_pos$y1[k],f_nazwy[k],col=k)
+	indx <- with(results,method_id==k & i<=10000)
+	with(results, lines(i[indx],time[indx],type="b",col=k))
+	text(text_pos$x1[k],text_pos$y1[k],f_names[k],col=k)
 }
 title("dayloop2 vs dayloop2_A",cex.main=1)
 dev.off()
-shell.exec(file.path(getwd(),"wer_1.png"))
+shell.exec(file.path(getwd(),"ver_1.png"))
 
-wyniki$czas[1:10]/wyniki$czas[11:20]
+results$time[1:10]/results$time[11:20]
 
-# Wykres 2
+# Plot 2
 m <- 1L:3L
-png("wer_2.png",540,360)
+png("ver_2.png",540,360)
 w_plot_init(100000, m)
 w_axis(10000)
 grid()
 for (k in m) {
-	indx <- with(wyniki,metoda_id==k & i<=100000)
-	with(wyniki, lines(i[indx],czas[indx],type="b",col=k))
-	text(text_pos$x2[k],text_pos$y2[k],f_nazwy[k],col=k)
+	indx <- with(results,method_id==k & i<=100000)
+	with(results, lines(i[indx],time[indx],type="b",col=k))
+	text(text_pos$x2[k],text_pos$y2[k],f_names[k],col=k)
 }
 title("Vectorization FTW",cex.main=1)
 dev.off()
-shell.exec(file.path(getwd(),"wer_2.png"))
+shell.exec(file.path(getwd(),"ver_2.png"))
 
-# Wykres 3
+# Plot 3
 m <- 3L:5L
-png("wer_3.png",540,360)
+png("ver_3.png",540,360)
 w_plot_init(850000, 3:5, n0=100000)
 w_axis(100000)
 grid()
 for (k in m) {
-	indx <- with(wyniki,metoda_id==k & i>=100000)
-	with(wyniki, lines(i[indx],czas[indx],type="b",col=k))
-	text(text_pos$x3[k],text_pos$y3[k],f_nazwy[k],col=k)
+	indx <- with(results,method_id==k & i>=100000)
+	with(results, lines(i[indx],time[indx],type="b",col=k))
+	text(text_pos$x3[k],text_pos$y3[k],f_names[k],col=k)
 }
 title("More vectorization",cex.main=1)
 dev.off()
-shell.exec(file.path(getwd(),"wer_3.png"))
+shell.exec(file.path(getwd(),"ver_3.png"))
 
-indx <- wyniki$metoda=="dayloop2_A"
-mdl <- lm(czas~i, wyniki, subset=indx)
+indx <- results$method=="dayloop2_A"
+mdl <- lm(time~i, results, subset=indx)
 est <- predict(mdl, data.frame(i=850000))
 # 6m12s
-   
-# Sekcja TESTÓW
+
+# Tests
 set.seed(2908822)
 n <- 8500#00
 temp <- as.data.frame(matrix(sample(1:10, n*9, TRUE), n, 9))
